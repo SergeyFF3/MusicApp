70a9d52714e70329cbba900a1499cb3efcd3660b
SergeyFF3 <sergey@redrazr.com>
2023-05-31 11:34:55 +0300

    add store, types, slices

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,10 @@
-import Actions from '@/components/Actions';
-import style from '@/styles/Home.module.scss';
 import MainLayout from '../layout/MainLayout';
 
 export default function Home() {
   return (
     <MainLayout>
-      <h2 className="test">Добро пожаловать!</h2>
+      <h2>Добро пожаловать!</h2>
       <h4>Здесь собраны лучшие треки.</h4>
-      <Actions />
     </MainLayout>
   );
 }
